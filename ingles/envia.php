b7eea0a9461010c1bf2561f14eb4e83013887fdb
PauloBaronista <paulo@rpm.com.br>
2020-11-18 13:47:49 -0300

    Ajuste layout

--- ingles/envia.php
+++ ingles/envia.php
@@ -3,14 +3,13 @@ $nome     = $_POST['nome'];
 $email    = $_POST['email'];
 $phone    = $_POST['phone'];
 $mensagem = $_POST['mensagem'];
-$headers = "From: <contato@portodomansomt.com.br>"."\n";
+$headers = "From: Porto do Manso <contato@portodomansomt.com.br>"."\n";
 $corpo  .= "Nome: ".$nome."<br/>";
 $corpo .= "E-mail: ".$email."<br/>";
 $corpo .= "Telefone: ".$phone. "<br/>";
 $corpo .= "Mensagem: ".$mensagem."<br/>";
 $headers .= "MIME-Version: 1.0"."\n";
 $headers .= "Content-type:text/html;charset=iso-8859-1"."\n";
-// mail("paulobaronista@gmail.com, renata@spicycomm.com.br, imoveisgalvao@gmail.com","Contato enviado pelo site www.portodomansomt.com.br",$corpo,$headers);
-mail("paulobaronista@gmail.com","Contato enviado pelo site www.portodomansomt.com.br",$corpo,$headers);
+mail("contato@portodomansomt.com.br, paulobaronista@gmail.com, renata@spicycomm.com.br, imoveisgalvao@gmail.com","Contato enviado pelo site www.portodomansomt.com.br",$corpo,$headers);
 ?>
  
