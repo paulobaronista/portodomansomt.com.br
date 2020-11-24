7c935888ebaa437eafc7c227607d1b3224fc8527
PauloBaronista <paulo@rpm.com.br>
2020-11-24 17:44:16 -0300

    cookies

--- js/site.js
+++ js/site.js
@@ -1,3 +1,24 @@
+$(document).ready(function () {
+
+  $('#cookies').show();
+  $('body').css('overflow', 'hidden');
+
+  // Check if the user already accepted it
+  if (window.localStorage.getItem('accept_cookies')) {
+      $('#cookies').hide();
+      $('body').css('overflow', 'auto');
+      return false;
+  }
+
+  $(".btn_cookie").click(function () {
+      // Save on LocalStorage
+      window.localStorage.setItem('accept_cookies', true);
+      $('#cookies').hide();
+      $('body').css('overflow', 'auto');
+      return false;
+  });
+});
+
 $(document).ready(function(){
   $("#abrir-zoom").on("click",function() {
     $('#poup-zoom').css("visibility","visible");
